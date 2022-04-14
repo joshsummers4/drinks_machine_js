3f0c9c88a8db0d7a01994459f3f2b31b6cb0c90b
joshsummers4 <67658350+joshsummers4@users.noreply.github.com>
2022-04-14 13:16:06 +0100

    Add files via upload

--- style/functions.js
+++ style/functions.js
@@ -1,28 +1,40 @@
 class Drink {
-    constructor(name, stepTwo, stepFour){
+    constructor(name, stepTwo, stepFour, picture){
         this.name = name;
         this.stepOne = "Boil some water";
         this.stepTwo = stepTwo;
         this.stepThree = this.pour();
         this.stepFour = stepFour;
+        this.picture = picture;
     }
     pour(){
         return "Pour " + this.name + " in the cup";
     }
     instructions(){
-        return `${this.stepOne} \n${this.stepTwo} \n${this.stepThree} \n${this.stepFour}`;
+        if (this.stepFour != ""){
+            return `<li>${this.stepOne}</li> <li>${this.stepTwo}</li> <li>${this.stepThree}</li> <li>${this.stepFour}</li>`;
+        } else {
+            return `<li>${this.stepOne}</li> <li>${this.stepTwo}</li> <li>${this.stepThree}</li>`;
+        }
+    }
+
+    pourDrink(){
+        document.getElementById('drinkOutput').style.display = 'block';
+        document.getElementById('drinkChoice').innerHTML = this.name;
+        document.getElementById('drinkInstructions').innerHTML = this.instructions();
+        document.getElementById('drinkImage').src = this.picture;
     }
 }
 
-const lemon = new Drink("Lemon Tea", "Steep the water in the tea", "Add lemon");
-const coffee = new Drink("Coffee", "Brew the coffee grounds", "Add sugar and milk");
-const chocolate = new Drink("Chocolate", "Add drinking chocolate powder to the water", "");
+const lemon = new Drink("Lemon Tea", "Steep the water in the tea", "Add lemon", './images/lemon.jpeg');
+const coffee = new Drink("Coffee", "Brew the coffee grounds", "Add sugar and milk", './images/coffee.jpg');
+const chocolate = new Drink("Chocolate", "Add drinking chocolate powder to the water", "", "./images/chocolate.jpg");
 
 
-function pourDrink(choice){
+/*function pourDrink(choice){
     document.getElementById('drinkOutput').style.display = 'block';
     let drinkName = choice.name;
     let instruction = choice.instructions();
     document.getElementById('drinkChoice').innerHTML = drinkName;
     document.getElementById('drinkInstructions').innerHTML = instruction;
-}
+}*/
